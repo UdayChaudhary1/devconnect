e3641771322786cfe7e19aeb6bcfc5b4888b4f1e
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-07-29 13:23:46 +0530

    Feat: add signup route and mount authRoutes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/connectDB.js";
 
+import authRoutes from "./routes/auth.js";
+
 dotenv.config();
 
 const app = express();
@@ -9,6 +11,8 @@ const app = express();
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
