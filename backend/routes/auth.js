94a2dc17b7a7bacc061439bb39e8b5e4e112557c
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-07-29 13:34:04 +0530

    Feat: add login and logout routes and controllers

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { signup } from "../controllers/auth.js";
+import { signup, login, logout } from "../controllers/auth.js";
 
 const router = express.Router();
 
 router.get("/signup", signup);
+router.get("/login", login);
+router.get("/logout", logout);
 
 export default router;
