21dd22ebe977e18bf1dd0d90b78471c70d7affb3
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-07-29 13:29:50 +0530

    Refactor: move signup handler to separate controller

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,8 @@
 import express from "express";
+import { signup } from "../controllers/auth.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-  res.send("Signup route");
-});
+router.get("/signup", signup);
 
 export default router;
