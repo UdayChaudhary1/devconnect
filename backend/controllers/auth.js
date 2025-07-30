f480b8c1ea2f31ce3613ab2e84be9d926003396b
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-07-30 12:53:04 +0530

    Add verification token and expiry to new user creation

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.js";
 import bcryptjs from "bcryptjs";
+import { generateVerificationCode } from "../utils/generateVerificationCode.js";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -14,10 +15,13 @@ export const signup = async (req, res) => {
       throw new Error("User already exists");
     }
     const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = generateVerificationCode();
     const user = new User({
       email,
       password: hashedPassword,
       name: name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000, //24 hours
     });
   } catch (error) {
     res.status(400).json({ success: false, message: error.message });
