566524d0c98bedb4b100d1582be1d04908cc0b22
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-08-01 14:23:02 +0530

    Add logic to send reset password email

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,4 +1,7 @@
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
@@ -40,5 +43,24 @@ export const sendWelcomeEmail = async (email, name) => {
   } catch (error) {
     console.log(`Error sending Welcome email: ${error}`);
     throw new Error(`Error sending Welcome email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password Reset",
+    });
+
+    console.log("Email Sent Successfully", response);
+  } catch (error) {
+    console.log(`Error sending password reset email: ${error}`);
+    throw new Error(`Error sending password reset email: ${error}`);
   }
 };
