69b658201c08bf8bb3b97af982f3bffdb90a3dfd
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-08-01 14:53:48 +0530

    Add logic to send email when user successfully reset the password

--- backend/mailtrap/email.js
+++ backend/mailtrap/email.js
@@ -1,5 +1,6 @@
 import {
   PASSWORD_RESET_REQUEST_TEMPLATE,
+  PASSWORD_RESET_SUCCESS_TEMPLATE,
   VERIFICATION_EMAIL_TEMPLATE,
 } from "./emailTemplates.js";
 import { mailtrapClient, sender } from "./mailtrap.js";
@@ -62,5 +63,24 @@ export const sendPasswordResetEmail = async (email, resetURL) => {
   } catch (error) {
     console.log(`Error sending password reset email: ${error}`);
     throw new Error(`Error sending password reset email: ${error}`);
+  }
+};
+
+export const sendResetSuccessEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Password Reset Successful",
+      html: PASSWORD_RESET_SUCCESS_TEMPLATE,
+      category: "Password Reset",
+    });
+
+    console.log("Password Reset Success Email Sent Successfully", response);
+  } catch (error) {
+    console.log(`Error sending password reset success email: ${error}`);
+    throw new Error(`Error sending password reset success email: ${error}`);
   }
 };
