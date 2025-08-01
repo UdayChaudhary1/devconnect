4fb980a90a49c7e4e0a8b837586624176ea99fbc
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-08-01 13:54:40 +0530

    Add a route for when user forgets password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,5 +1,11 @@
 import express from "express";
-import { signup, login, logout, verifyEmail } from "../controllers/auth.js";
+import {
+  signup,
+  login,
+  logout,
+  verifyEmail,
+  forgotPassword,
+} from "../controllers/auth.js";
 
 const router = express.Router();
 
@@ -7,5 +13,6 @@ router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
 router.post("/verify-email", verifyEmail);
+router.post("/forgot-password", forgotPassword);
 
 export default router;
