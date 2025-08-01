8d0243394958df279a5181564e3aced56f698156
Uday Chaudhary <chaudharyuday081@gmail.com>
2025-08-01 15:20:02 +0530

    Add cookie parser middleware

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./config/connectDB.js";
 
 import authRoutes from "./routes/auth.js";
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
